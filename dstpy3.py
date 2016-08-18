1bf031b19b23a79dcfb28ebfa716ce9cef35f816
xmhk <chmhnk@googlemail.com>
2016-08-18 11:21:17 +0200

    typos, bugfx

--- dstpy3.py
+++ dstpy3.py
@@ -6,8 +6,8 @@ import numpy.linalg as linalg
 #the DST object and the algorithms implemented here are heavily based by the following papers
 #
 # G. BOFFETTA AND A. R. OSBORNE	 Computation of the Direct Scattering Transform for the Nonlineare Schroedinger Equation
-#								Journal of Comp Phys 102, p 252-264, (1992)	  
-#								http://personalpages.to.infn.it/~boffetta/Papers/bo92.pdf
+#								 Journal of Comp Phys 102, p 252-264, (1992)	  
+#								 http://personalpages.to.infn.it/~boffetta/Papers/bo92.pdf
 #
 #
 #
@@ -41,45 +41,45 @@ class DSTObj():
 		self.scaled_dom = np.pi / (2 * self.L)
 		self.ommax = np.floor( npoints / 2. ) * self.scaled_dom
 		
-		self.calc_ab_methodsdict = {'TMC': calc_ab_transfermatrix_clib,
-									'TM' : calc_ab_transfermatrix_vanilla,
-									'CD' : calc_ab_centraldifference_vanilla,
-									'CN' : calc_ab_cranknicolson_vanilla,
-									'AL' : calc_ab_ablowitzladik_vanilla,
-									'AL2': calc_ab_ablowitzladik2_vanilla,
-									'FD' : calc_ab_forwarddisc_vanilla,
-									'FDC' : calc_ab_forwarddisc_clib,
-									'RK4' : calc_ab_rungekutta4_vanilla, 
+		self.calc_ab_methodsdict = {'TMC'  : calc_ab_transfermatrix_clib,
+									'TM'   : calc_ab_transfermatrix_vanilla,
+									'CD'   : calc_ab_centraldifference_vanilla,
+									'CN'   : calc_ab_cranknicolson_vanilla,
+									'AL'   : calc_ab_ablowitzladik_vanilla,
+									'AL2'  : calc_ab_ablowitzladik2_vanilla,
+									'FD'   : calc_ab_forwarddisc_vanilla,
+									'FDC'  : calc_ab_forwarddisc_clib,
+									'RK4'  : calc_ab_rungekutta4_vanilla, 
 									'RK4C' : calc_ab_rungekutta4_clib
 									}		
 											
-		self.calc_ab_methodnamesdict =	 {'TMC': 'Transfer Matrix (C)',
-										  'TM' : 'Transfer Matrix (Python)',
-										  'CD' : 'Central Discretization (Python)',
-										  'CN' : 'Crank Nicolson (Python)',
-										  'AL' : 'Ablowitz Ladik (Python)',
-										  'AL2': 'Ablowitz Ladik Norm (Python)',
-										  'FD' : 'Forward Discretization (Python)',										  
-										  'FDC' : 'Forward Discretization (C)', 
-										  'RK4' : 'Runge-Kutta 4 (Python)',
+		self.calc_ab_methodnamesdict =	 {'TMC'  : 'Transfer Matrix (C)',
+										  'TM'   : 'Transfer Matrix (Python)',
+										  'CD'   : 'Central Discretization (Python)',
+										  'CN'   : 'Crank Nicolson (Python)',
+										  'AL'   : 'Ablowitz Ladik (Python)',
+										  'AL2'  : 'Ablowitz Ladik Norm (Python)',
+										  'FD'   : 'Forward Discretization (Python)',										  
+										  'FDC'  : 'Forward Discretization (C)', 
+										  'RK4'  : 'Runge-Kutta 4 (Python)',
 										  'RK4C' : 'Runge-Kutta 4 (Python)'}
 										  
-		self.calc_abdiff_methodsdict = {#'TMC' : calc_ab_diff_transfermatrix_clib,
-										#'FDC' : calc_ab_diff_forwarddiff_clib,
-										#'RK4C' : calc_ab_diff_rk4_clib,
-										#'RK4' :  calc_abdiff_rungekutta_vanilla,
-										#'FD'  : calc_ab_diff_forwarddisc_vanilla,
-										'AL'  : calc_ab_diff_ablowitzladik_vanilla,
-										'ALC'  : calc_al_diff_clib}
+		self.calc_abdiff_methodsdict = {#'TMC'   : calc_ab_diff_transfermatrix_clib,    # commented versions only exists as scratch
+										#'FDC'   : calc_ab_diff_forwarddiff_clib,
+										#'RK4C'  : calc_ab_diff_rk4_clib,
+										#'RK4'   :  calc_abdiff_rungekutta_vanilla,
+										#'FD'    : calc_ab_diff_forwarddisc_vanilla,
+										'AL'     : calc_ab_diff_ablowitzladik_vanilla,
+										'ALC'    : calc_al_diff_clib}
 										
 		self.calc_abdiff_methodnamesdict = {#'TMC' : 'Transfer Matrix (C)',
-										#'FDC' : 'Forward Discretization (C)',
-										#'FD'  : 'Forward Discretization (Python)',
-										'AL'  : 'Ablowitz Ladik (Python)',
-										'ALC'  : 'Ablowitz Ladik (C)',
-										#'RK4C' : 'Runge Kutta 4 (C)',
-										#'RK4' : 'Runge Kutta 4 (Python)'
-										}		
+										    #'FDC' : 'Forward Discretization (C)',
+										    #'FD'  : 'Forward Discretization (Python)',
+										     'AL'  : 'Ablowitz Ladik (Python)',
+										    'ALC'  : 'Ablowitz Ladik (C)',
+										    #'RK4C' : 'Runge Kutta 4 (C)',
+										    #'RK4' : 'Runge Kutta 4 (Python)'
+                                            }		
 
 												
 										  
@@ -93,7 +93,7 @@ class DSTObj():
 		for k in self.calc_abdiff_methodsdict:
 			print("							  %s : %s"%(k, self.calc_abdiff_methodnamesdict[k]))		
 
-		print("\neigenvalue calculation: use .calc_evals() of .calc_evals_cdm BUT this is slow.")
+		print("\neigenvalue calculation: use .calc_evals() / .calc_evals_spec() BUT this is slow and buggy.")
 	
 	
 	def calc_ab(self,zetas, method = 'RK4C'):
@@ -130,9 +130,30 @@ class DSTObj():
 				bd = bd[0]
 		return a,b, ad, bd		
 
-  
+
+	def calc_evals(self):  
+        #method based on central difference matrix 
+		len_q = len(self.q)
+		cdm = np.zeros( [len_q,len_q], dtype=complex)
+		for i in range(1, len_q):
+			cdm[i,i-1] = -1
+			cdm[i-1,i] = 1
+		cdm[0, len_q-1] = -1
+		cdm[len_q-1, 0] = 1
+		cdm = cdm / 2. / self.dx
+        
+		MM = np.zeros([2*len_q, 2*len_q], dtype=complex)
+		MM[    0:len_q,        0:len_q  ] = cdm
+		MM[    0:len_q,    len_q:2*len_q] = -np.diag(self.q)
+        MM[len_q:2*len_q,      0:len_q  ] = -np.diag(np.conj(self.q))
+        MM[len_q:2*len_q,  len_q:2*len_q] = -cdm		
+		
+		MM = 1.0j * MM	  
+		evals, evecs = linalg.eig(MM)
+		return evals
+        
 	
-	def calc_evals(self):
+	def calc_evals_spec(self):
 		#matrix spectral method after Yousefi
 		def matrixGAMMA(vGAMMA):
 			LGAMMA2 = np.int(	len(vGAMMA)/2)
@@ -144,17 +165,17 @@ class DSTObj():
 				ii = mstart
 				gammi = gstart
 				while ii<mend:
-				  GAMMA[row, ii] = vGAMMA[gammi]
+				  GAMMA[row, ii] = -1.0j * vGAMMA[gammi]
 				  ii+=1
 				  gammi+=-1	  
 			return GAMMA		
-
+    
 		qft   = np.fft.ifft(self.q)
 		M     = len(self.q)
 		M2    = int( M/2.0)
 		Gamma = matrixGAMMA(qft)
-		Omega = 2*np.pi / 2/self.L * np.diag(np.arange(-M2,M2))	
-		
+		Omega = -2*np.pi / 2/self.L * np.diag(np.arange(-M2,M2))	
+	
 		matrixA = np.zeros([2*M,2*M], dtype=complex)
 		matrixA[0:M, 0:M]    = Omega
 		matrixA[0:M, M:2*M]  = Gamma
@@ -163,22 +184,6 @@ class DSTObj():
 		evals, evecs = linalg.eig(matrixA)
 		return evals	
 		
-	def calc_evals_cdm(self):  #method based on central difference matrix 
-		len_q = len(self.q)
-		cdm = np.zeros( [len_q,len_q], dtype=complex)
-		for i in range(1, len_q):
-			cdm[i,i-1] = -1
-			cdm[i-1,i] = 1
-			cdm[0, len_q-1] = -1
-			cdm[len_q-1, 0] = 1
-		cdm = cdm / 2. /self.dx
-		MM = np.zeros([2*len_q, 2*len_q], dtype=complex)
-		MM[0:len_q, 0:len_q]=cdm
-		MM[len_q:2*len_q,len_q:2*len_q] = -cdm
-		MM[len_q:2*len_q, 0:len_q] = -np.diag(np.conj(self.q))
-		MM[0:len_q, len_q:2*len_q]=	 -np.diag(self.q)
-		MM = 1.0j * MM	  
-		evals, evecs = linalg.eig(MM)
-		return evals
+
 			
 
